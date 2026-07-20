fe534c680a79309af066511ba997ccd0aecd4bab
Trần Nguyễn Quỳnh Như <trannguyenquynhnhu24@gmail.com>
2026-07-20 09:59:03 +0000

    update database

--- app/core/database.py
+++ app/core/database.py
@@ -2,15 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
+from app.core.database import get_db 
 
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
